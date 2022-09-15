c3eef62f7427f6dab68d4429a28be7636f5efd74
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-09-14 18:17:30 -0700

    Algorithms - Variable

---
+++ Algorithms-Course/1 Variables.js
@@ -0,0 +1,44 @@
+
+
+// How you can create variables
+
+// Using Let Keyword
+let userName = "Prabh";
+
+
+// Using Var keyword
+var age = 27;
+
+
+// Using const keyword
+const collegeName = "CICCC";
+
+
+
+// Any programming language, needs an environment to execute
+
+// How can we run javascript (2 Ways)
+
+
+// 1. BROWSER ****************
+// Using Browser because it have pre installed compiler called as V8 Engline
+// Earlier Javascript used to only work on browsers 
+
+// Other Browsers have other engines 
+// Mozilla Firefox uses (SpiderMonkey)
+// Google Chrome uses (V8 Engine)
+// Edge uses Chakra
+
+
+
+// 2. Node.js **************
+// Node.js is a runtime environment which make it possible to run javascript anywhere other than browser
+
+
+// (The developer got inspired from V8 Engine and made Node.js for Any platform)
+
+
+
+console.log(userName);
+console.log(age);
+console.log(collegeName);
