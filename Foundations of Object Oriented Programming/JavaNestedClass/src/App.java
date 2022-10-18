f5c165d1e4966bd72661be81c920edfd28a4e487
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-10-17 20:12:03 -0700

    java abstraction and interfaces and nested class

---
+++ Foundations of Object Oriented Programming/JavaNestedClass/src/App.java
@@ -0,0 +1,11 @@
+import Vehicle.Vehicle;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Vehicle v1 = new Vehicle();
+
+        v1.getVehiclePart();
+    }
+}
---
+++ Foundations of Object Oriented Programming/JavaNestedClass/src/Vehicle/Vehicle.java
@@ -0,0 +1,52 @@
+package Vehicle;
+
+public class Vehicle {
+    private String name;
+    private String engine;
+    private String fuelTank;
+
+    public Vehicle () {
+
+    }
+
+    public Vehicle(String name, String engine, String fuelTank) {
+        this.name = name;
+        this.engine = engine;
+        this.fuelTank = fuelTank;
+    }
+
+    // Nested Class 
+    class VehiclePart {
+        private String partName;
+        private String partPrice;
+    
+
+        public VehiclePart(String partName, String partPrice) {
+            this.partName = partName;
+            this.partPrice = partPrice;
+        }
+
+        public String getPartName() {
+            return partName;
+        }
+
+        public void setPartName(String partName) {
+            this.partName = partName;
+        }
+
+        public String getPartPrice() {
+            return partPrice;
+        }
+
+        public void setPartPrice(String partPrice) {
+            this.partPrice = partPrice;
+        }
+
+
+    }
+    
+    public void getVehiclePart() {
+        VehiclePart part1 = new VehiclePart("Steering", "$200");
+        System.out.println(part1.partName);
+    }
+}
