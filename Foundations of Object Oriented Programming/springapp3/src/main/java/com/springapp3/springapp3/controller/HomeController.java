33ec7eaf1d6ed5f3d4ed7f0477820f5ac0056730
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-11-15 19:59:35 -0800

    trees basic

--- Foundations of Object Oriented Programming/springapp3/src/main/java/com/springapp3/springapp3/controller/HomeController.java
+++ Foundations of Object Oriented Programming/springapp3/src/main/java/com/springapp3/springapp3/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.springapp3.springapp3.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.springapp3.springapp3.dao.iProjectRepository;
+import com.springapp3.springapp3.entity.Project;
 
 @Controller
 public class HomeController {
     
+    // We use autowired because we don't want to write our implmentations for now of CRUD METHODS
+    @Autowired
+    iProjectRepository projectRepository;
+
+
     @GetMapping("/")
-    public String displayHome() {
+    public String displayHome(Model model) {
+        List<Project> projects = projectRepository.findAll();
+        model.addAttribute("projects", projects);
         return "home/index";
     }
 }
--- ProgrammingQuestions/Trees/Tree.js
+++ ProgrammingQuestions/Trees/Tree.js
@@ -16,44 +16,77 @@ node2.left = node4;
 node2.right = node5;
 node3.left = node6;
 node3.right = node7;
-//         // 10 
+//         // 10
 // .    20              30
 // 40       50      60      70
 
 console.log(node1); // Root
 
-
 // TRAVERSING A TREE CAN BE POSSIBLE IN MUTLIPLE WAYS
 
 // Preorder Traversal
-    // Recursion ******************************
+// VISIT ROOT NODE FIRST
+// VIST LEFT SUBTREE
+// VISIT RIGHT SUBTREE
+// Recursion ******************************
 
-    function preOrderRecursion(root) {
+function preOrderRecursion(root) {
+  //         // 10
+  // .    20              30
+  // 40       50      60      70
 
-        //         // 10 
-// .    20              30
-// 40       50      60      70
+  if (root === null) {
+    return;
+  }
 
-
-        if (root === null) {
-            return;
-        }
-
-        console.log(root.val + " --> ");
-        preOrderRecursion(root.left);
-        preOrderRecursion(root.right);
-    }
-
+  console.log(root.val + " --> ");
+  preOrderRecursion(root.left);
+  preOrderRecursion(root.right);
+}
 
 // In Order Traversal
-
+// VIST LEFT SUBTREE
+// VISIT ROOT NODE FIRST
+// VISIT RIGHT SUBTREE
+function inorderRecursion(root) {
+    //         // 10
+    // .    20              30
+    // 40       50      60      70
+  
+    if (root === null) {
+      return;
+    }
+  
+    inorderRecursion(root.left);
+    console.log(root.val + " --> ");
+    inorderRecursion(root.right);
+  }
+  
 
 // PostOrder Traversal
 
 
+// In Order Traversal
+// VIST LEFT SUBTREE
+// VISIT ROOT NODE FIRST
+// VISIT RIGHT SUBTREE
+function postOrderRecursion(root) {
+    //         // 10
+    // .    20              30
+    // 40       50      60      70
+  
+    if (root === null) {
+      return;
+    }
+  
+    postOrderRecursion(root.left);
+    postOrderRecursion(root.right);
+    console.log(root.val + " --> ");
+
+  }
+
 // level Order Traversal
 
+// preOrderRecursion(node1);
 
-
-
-preOrderRecursion(node1);
+postOrderRecursion(node1);
