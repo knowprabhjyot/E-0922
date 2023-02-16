9bf58fefa4c1c8d60e7e718b31acab8ec178b23d
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2023-02-15 20:48:19 -0800

    tmdb movies

--- Web Dev II/Week 4/Day 1 (TMDB Movie)/index.js
+++ Web Dev II/Week 4/Day 1 (TMDB Movie)/index.js
@@ -8,23 +8,11 @@ let popularMovies = [];
 let topRatedMovies = [];
 let upcomingMovies = [];
 
-// LOADING GLIDE DEPENDENCIES
-new Glide(".glide", {
-  type: "carousel",
-  perView: 1,
-  focusAt: "center",
-  autoPlay: 1000,
-}).mount();
-
-// glide.mount();
-
 // URLS
 const TRENDING_MOVIES_URL = `${config.baseUrl}/trending/all/day?api_key=${config.apiKey}`;
 const POPULAR_MOVIES_URL = `${config.baseUrl}/movie/popular?api_key=${config.apiKey}`;
 const TOP_RATED_MOVIES_URL = `${config.baseUrl}/movie/top_rated?api_key=${config.apiKey}`;
 const UPCOMING_MOVIES_URL = `${config.baseUrl}/movie/upcoming?api_key=${config.apiKey}`;
-
-// window.addEventListener("load", async () => {
 
 /**
  *
@@ -35,17 +23,22 @@ const getMovies = async (url) => {
   return await axios.get(url);
 };
 
-const generateBannerUI = async () => {
-  const slidesContainer = document.getElementById("glidesSlides"); // ul
-  let { data } = await getMovies(UPCOMING_MOVIES_URL);
-
-  upcomingMovies = data.results.map((movie) => {
+const manipulateArray = (data) => {
+  return data.results.map((movie) => {
     return {
       title: movie.title,
       posterUrl: `https://image.tmdb.org/t/p/original/${movie.poster_path}`,
       description: movie.overview,
     };
   });
+};
+
+const generateBannerUI = async () => {
+  const slidesContainer = document.getElementById("glidesSlides"); // ul
+  let { data } = await getMovies(UPCOMING_MOVIES_URL);
+
+  console.log(data, "data in generate Banner");
+  upcomingMovies = manipulateArray(data);
 
   upcomingMovies.forEach((movie) => {
     const li = document.createElement("li");
@@ -55,21 +48,88 @@ const generateBannerUI = async () => {
     const img = document.createElement("img");
     img.src = movie.posterUrl;
 
-    div.appendChild(img);
+    const textContainer = document.createElement("div");
+
+    const title = document.createElement("h1");
+    title.textContent = movie.title;
+
+    const description = document.createElement("p");
+    description.textContent = movie.description;
+    textContainer.append(title, description);
+
+    div.classList.add("banner-container");
+    div.append(img, textContainer);
+
     li.appendChild(div);
 
     slidesContainer.appendChild(li);
+
+    // LOADING GLIDE DEPENDENCIES
+    new Glide(".glide", {
+      type: "carousel",
+      perView: 1,
+      focusAt: "center",
+      // autoPlay: 1000,
+    }).mount();
   });
 };
 
-(async function loadResources() {
-  try {
-    trendingMovies = await getMovies(TRENDING_MOVIES_URL);
-    popularMovies = await getMovies(POPULAR_MOVIES_URL);
-    topRatedMovies = await getMovies(TOP_RATED_MOVIES_URL);
+const generateMovieUI = async () => {
+  let { data } = await getMovies(TRENDING_MOVIES_URL);
+  console.log(data, "data in generate Movie UI");
 
+  trendingMovies = manipulateArray(data);
+
+  let trendingMovieContainer = document.getElementById("trendingMovies");
+
+  trendingMovies.map((trendingMovie) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = trendingMovie.posterUrl;
+
+    li.appendChild(img);
+    trendingMovieContainer.appendChild(li);
+  });
+
+  let popularMoviesResponse = await getMovies(POPULAR_MOVIES_URL);
+
+  popularMovies = manipulateArray(popularMoviesResponse.data);
+
+  let popularMovieContainer = document.getElementById("popularMovies");
+  popularMovies.map((popularMovie) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = popularMovie.posterUrl;
+
+    li.appendChild(img);
+    popularMovieContainer.appendChild(li);
+  });
+
+  // LOADING GLIDE DEPENDENCIES
+  new Glide(".glide-section", {
+    type: "carousel",
+    perView: 4,
+    focusAt: "center",
+    // autoPlay: 1000,
+  }).mount();
+
+  new Glide(".glide-section2", {
+    type: "carousel",
+    perView: 4,
+    focusAt: "center",
+    // autoPlay: 1000,
+  }).mount();
+
+  topRatedMovies = await getMovies(TOP_RATED_MOVIES_URL);
+};
+
+async function loadResources() {
+  try {
     generateBannerUI();
+    generateMovieUI();
   } catch (error) {
     alert(error);
   }
-})();
+}
+
+loadResources();
