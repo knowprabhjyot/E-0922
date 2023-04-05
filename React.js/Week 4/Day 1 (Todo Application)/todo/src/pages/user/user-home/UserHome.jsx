126f1c73735a8f20d04002908aa5ff9fd4eefa33
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2023-04-04 19:32:50 -0700

    todos ui

--- React.js/Week 4/Day 1 (Todo Application)/todo/src/pages/user/user-home/UserHome.jsx
+++ React.js/Week 4/Day 1 (Todo Application)/todo/src/pages/user/user-home/UserHome.jsx
@@ -1,10 +1,12 @@
-import { AppBar, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Fab, IconButton, TextField, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Fab, IconButton, TextField, ToggleButton, ToggleButtonGroup, Toolbar, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import CustomButton from '../../../components/CustomButton';
 import AddIcon from '@mui/icons-material/Add';
 import { Box } from '@mui/system';
+import CustomCard from '../../../components/CustomCard';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function UserHome() {
 
@@ -13,9 +15,14 @@ export default function UserHome() {
     const [openDialog, setOpenDialog] = useState(false);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [priority, setPriority] = useState("low");
+
+    const [todosList, setTodosList] = useState([]);
+    const [newTaskCreated, setNewTaskCreated] = useState({});
 
     useEffect(() => {
         const currentuser = JSON.parse(localStorage.getItem('current-user'));
+
 
         if (!currentuser) {
             navigate('/user/login');
@@ -24,22 +31,44 @@ export default function UserHome() {
         }
     }, []);
 
+    useEffect(() => {
+        const todos = JSON.parse(localStorage.getItem('todoList'));
+
+        if (todos) {
+            setTodosList(todos);
+        }
+
+    }, [newTaskCreated]);
+
+
+    const markTaskComplete = () => {
+        alert("Fired");
+    }
+
 
     const handleClose = () => {
         setOpenDialog(false);
     }
 
     const handleSubmitTodo = () => {
-      const newTodo = {
-        title,
-        description
-      }
-      const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
-      todoList.push(newTodo);
+        const newTodo = {
+            title,
+            description,
+            id: uuidv4(),
+            isCompleted: false,
+            priority
+        }
 
-      localStorage.setItem('todoList', JSON.stringify(todoList));
+        setNewTaskCreated(newTodo);
+        const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+        todoList.push(newTodo);
 
-      handleClose();
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+
+        setTitle("");
+        setDescription("");
+
+        handleClose();
     }
 
     return (
@@ -74,11 +103,43 @@ export default function UserHome() {
                         <TextField value={description} onChange={(e) => setDescription(e.target.value)} variant="outlined" label="Description" type="text" placeholder="Enter Todo Description" />
                     </Box>
                 </DialogContent>
+                <Box margin="auto">
+                    <ToggleButtonGroup
+                        color="primary"
+                        value={priority}
+                        exclusive
+                        onChange={(e, prioritySelected) => setPriority(prioritySelected)}
+                        aria-label="Platform"
+                    >
+                        <ToggleButton size='small' value="low">Low</ToggleButton>
+                        <ToggleButton size='small' value="medium">Medium</ToggleButton>
+                        <ToggleButton size='small' value="high">High</ToggleButton>
+
+                    </ToggleButtonGroup>
+                </Box>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
                     <Button onClick={handleSubmitTodo}>Submit</Button>
                 </DialogActions>
             </Dialog>
+
+            {/* TODOS --> ARRAY[TODO] */}
+
+
+
+            {
+                todosList.length === 0 ? <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+                    <Typography>No Tasks added yet</Typography>
+                </Box> : <Box display="grid" gridTemplateColumns="repeat(4, 1fr)" gap="8px" margin="20px">
+                    {
+                        todosList.map((todoItem) => {
+                            return <CustomCard setComplete={markTaskComplete} todo={todoItem} />
+                        })
+                    }
+                </Box>
+            }
+
+
 
             <Box position="fixed" bottom="30px" right="30px">
                 <Fab onClick={() => setOpenDialog(true)} size="small" color="secondary" aria-label="add">
